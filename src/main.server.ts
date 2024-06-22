c3ac97c78404a83863968d3102152f86d1afe558
hironmoy27 <hchakraborty2708@gmail.com>
2024-06-22 11:39:37 +0530

    initial setup complete

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,34 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { config } from './app/app.config.server';
+import 'zone.js/node';
+import { enableProdMode } from '@angular/core';
+import { ngExpressEngine } from '@nguniversal/express-engine';
+import { APP_BASE_HREF } from '@angular/common';
+import { join } from 'path';
+import * as express from 'express';
+import { existsSync } from 'fs';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+import { AppServerModule } from './app/app.server.module';
+import { environment } from './environment/environment';
 
-export default bootstrap;
+if (environment.production) {
+  enableProdMode();
+}
+
+export const app = express();
+
+const distFolder = join(process.cwd(), 'dist/project_1/browser');
+const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index.html';
+
+app.engine('html', ngExpressEngine({
+  bootstrap: AppServerModule,
+}));
+
+app.set('view engine', 'html');
+app.set('views', distFolder);
+
+app.get('*.*', express.static(distFolder, {
+  maxAge: '1y'
+}));
+
+app.get('*', (req, res) => {
+  res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+});
---
+++ src/polyfills.ts
@@ -0,0 +1,8 @@
+/** IE10 and IE11 requires the following for NgClass support on SVG elements */
+// import 'classlist.js';  // Run `npm install --save classlist.js`.
+
+import 'zone.js';  // Included with Angular CLI.
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
